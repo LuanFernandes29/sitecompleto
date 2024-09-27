6156d1d5dccb067645df5ae505b4c2d3d292a84a
LuanFernandes29 <gamerscaua@gmail.com>
2024-09-27 07:32:40 -0300

    a

--- script.js
+++ script.js
@@ -15,21 +15,20 @@ const form = document.getElementById('login_form')
         form.classList.remove('dark') 
     }
 })
-function disableoptions(questionName) {
-    let options = document.getElementsByName(questionName);
+function disableOptions(questionName){
+    let options = document.getElementsByName (questionName);
     options.forEach(option => {
-        if (!option.checked) {
+        if(!option.checked){
             option.disabled = true;
         }
     });
 }
-
-function playsound() {
-    let clicksound = document.getElementById('selecionasom');
-    clicksound.play();
+function playSound(){
+    let clickSound = document.getElementById('selecionasom');
+    clickSound.play();
 }
-
-function submitquiz() {
+function submitQuiz(){
+ 
     let correctAnswers = {
         q1: "B",  // Ecologia
         q2: "D",  // Cosméticos naturais
@@ -42,29 +41,36 @@ function submitquiz() {
         q9: "A",  // Feitos com ingredientes naturais e não poluem o ambiente
         q10: "B"  // Redução de consumo de água e energia
     };
-
     let form = document.getElementById('quiz-form');
     let score = 0;
-
-    for (let key in correctAnswers) {
+ 
+    for (let key in correctAnswers){
         let userAnswer = form.elements[key].value;
-        if (userAnswer === correctAnswers[key]) {
+        if(userAnswer === correctAnswers[key]){
             score++;
         }
     }
-
     let result = document.getElementById('result');
-    result.innerHTML = `Você acertou ${score} de 10 perguntas`;
-
-    if (score === 10) {
-        let sucessSound = document.getElementById('venceusom');
+    result.innerHTML = `Você acertou ${score} de 7 perguntas.`
+ 
+    if(score === 7){
+        let sucessSound = document.getElementById("venceusom");
         sucessSound.play();
+    }else{
+        let failSound = document.getElementById("perdeusom");
+        failSound.play();
     }
-    else{
-        let perdeusound = document.getElementById('perdeusom');
-        perdeusound.play()
-    }
+    document.getElementById('enviar').disabled=true;
+    document.getElementById('recomeco').disabled=false;
 }
-
-
+function recomecar(){
+    let form = document.getElementById('quiz-form');
+    form.reset();
+    let options = document.querySelectorAll('input[type="radio"]');
+    options.forEach(option => option.disabled=false);
+    score = 0;
+    result.innerHTML = "";
+    document.getElementById('enviar').disabled=false;
+    document.getElementById('recomeco').disabled=true;
+};
  
